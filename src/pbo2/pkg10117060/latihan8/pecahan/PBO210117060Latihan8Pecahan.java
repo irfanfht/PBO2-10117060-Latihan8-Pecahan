084dfaa0ee530dfb3b05bfd2482c38a87e23056c
kekerofficial <43506956+kekerofficial@users.noreply.github.com>
2018-09-23 12:28:35 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117060/latihan8/pecahan/PBO210117060Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117060.latihan8.pecahan;
+
+/**
+ *
+* @author 
+ * NAMA : Irfan Faisal Hutama Tanjung.
+ * KELAS : PBO2
+ * NIM : 10117060
+ * Deskripsi Program : Program ini berisi tentang jenis tipe data 
+ * bilangan pecahan
+ */
+public class PBO210117060Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
